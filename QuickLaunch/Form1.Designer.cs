b1769c2dbc4a780474e10b7c9b25a870c07d083a
JensUbrich <JensUbrich@HERBST-GAME01>
2025-02-24 23:46:03 +0100

    Projektdateien hinzufügen.

---
+++ QuickLaunch/Form1.Designer.cs
@@ -0,0 +1,71 @@
+﻿namespace QuickLaunch
+{
+    partial class MainForm
+    {
+        private System.ComponentModel.IContainer components = null;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel;
+        private System.Windows.Forms.ToolStrip toolStrip;
+        private System.Windows.Forms.ToolStripButton addProgramButton;
+        private System.Windows.Forms.ToolStripButton exitButton;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            this.flowLayoutPanel = new System.Windows.Forms.FlowLayoutPanel();
+            this.toolStrip = new System.Windows.Forms.ToolStrip();
+            this.addProgramButton = new System.Windows.Forms.ToolStripButton();
+            this.exitButton = new System.Windows.Forms.ToolStripButton();
+            this.SuspendLayout();
+            // 
+            // flowLayoutPanel
+            // 
+            this.flowLayoutPanel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.flowLayoutPanel.AutoScroll = true;
+            this.flowLayoutPanel.FlowDirection = System.Windows.Forms.FlowDirection.TopDown; // Anordnung von oben nach unten
+            this.flowLayoutPanel.WrapContents = false; // Kein Zeilenumbruch
+            this.flowLayoutPanel.Padding = new System.Windows.Forms.Padding(0, 50, 0, 0); // Abstand hinzufügen
+            this.Controls.Add(this.flowLayoutPanel);
+            // 
+            // toolStrip
+            // 
+            this.toolStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.addProgramButton,
+            this.exitButton});
+            this.toolStrip.Dock = System.Windows.Forms.DockStyle.Top;
+            this.Controls.Add(this.toolStrip);
+            // 
+            // addProgramButton
+            // 
+            this.addProgramButton.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.addProgramButton.Image = Properties.Resources.gear_icon; // Zahnrad-Icon hinzufügen
+            this.addProgramButton.Click += new System.EventHandler(this.AddProgramButton_Click);
+            // 
+            // exitButton
+            // 
+            this.exitButton.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.exitButton.Image = Properties.Resources.exit_icon; // Exit-Icon hinzufügen
+            this.exitButton.Click += new System.EventHandler(this.ExitButton_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 600);
+            this.Controls.Add(this.toolStrip);
+            this.Controls.Add(this.flowLayoutPanel);
+            this.Name = "MainForm";
+            this.Text = "Schnellstart";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.MainForm_FormClosing);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+    }
+}
---
+++ QuickLaunch/Form1.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Windows.Forms;
+using System.Xml.Serialization;
+
+namespace QuickLaunch
+{
+
+    public partial class MainForm : Form
+    {
+        private List<QuickLaunchItem> quickLaunchItems = new List<QuickLaunchItem>();
+        private const string SaveFilePath = "quicklaunchitems.xml";
+
+        public MainForm()
+        {
+            InitializeComponent();
+            LoadQuickLaunchItems();
+        }
+
+        private void LoadQuickLaunchItems()
+        {
+            if (File.Exists(SaveFilePath))
+            {
+                try
+                {
+                    XmlSerializer serializer = new XmlSerializer(typeof(List<QuickLaunchItem>));
+                    using (FileStream fs = new FileStream(SaveFilePath, FileMode.Open))
+                    {
+                        quickLaunchItems = (List<QuickLaunchItem>)serializer.Deserialize(fs);
+                    }
+                    DisplayQuickLaunchItems();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"Fehler beim Laden der QuickLaunch-Items: {ex.Message}");
+                }
+            }
+        }
+
+        private void SaveQuickLaunchItems()
+        {
+            XmlSerializer serializer = new XmlSerializer(typeof(List<QuickLaunchItem>));
+            using (FileStream fs = new FileStream(SaveFilePath, FileMode.Create))
+            {
+                serializer.Serialize(fs, quickLaunchItems);
+            }
+        }
+
+        private void AddProgramButton_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog
+            {
+                Filter = "Programme (*.exe)|*.exe",
+                Title = "Programm hinzufügen"
+            };
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                string filePath = openFileDialog.FileName;
+                string fileName = Path.GetFileNameWithoutExtension(filePath);
+                QuickLaunchItem item = new QuickLaunchItem
+                {
+                    FilePath = filePath,
+                    FileName = fileName
+                };
+                quickLaunchItems.Add(item);
+                DisplayQuickLaunchItems();
+            }
+        }
+
+        private void RemoveProgramButton_Click(object sender, EventArgs e, QuickLaunchItem item)
+        {
+            quickLaunchItems.Remove(item);
+            DisplayQuickLaunchItems();
+        }
+
+        private void DisplayQuickLaunchItems()
+        {
+            flowLayoutPanel.Controls.Clear();
+            foreach (var item in quickLaunchItems)
+            {
+                FlowLayoutPanel itemPanel = new FlowLayoutPanel
+                {
+                    FlowDirection = FlowDirection.LeftToRight,
+                    Width = flowLayoutPanel.ClientSize.Width - 25, // Platz für Scrollbar
+                    Height = 30 // Kleinere Höhe
+                };
+                Button programButton = new Button
+                {
+                    Text = item.FileName,
+                    Width = 100, // Kleinere Breite
+                    Height = 30 // Kleinere Höhe
+                };
+                programButton.Click += (s, e) => System.Diagnostics.Process.Start(item.FilePath);
+                Button removeButton = new Button
+                {
+                    Text = "Löschen",
+                    Width = 60,
+                    Height = 30
+                };
+                removeButton.Click += (s, e) => RemoveProgramButton_Click(s, e, item);
+                itemPanel.Controls.Add(programButton);
+                itemPanel.Controls.Add(removeButton);
+                flowLayoutPanel.Controls.Add(itemPanel);
+                // Trennlinie hinzufügen
+                Label separator = new Label
+                {
+                    BorderStyle = BorderStyle.Fixed3D,
+                    Height = 2,
+                    Width = flowLayoutPanel.ClientSize.Width - 25
+                };
+                flowLayoutPanel.Controls.Add(separator);
+            }
+            flowLayoutPanel.PerformLayout(); // Layout aktualisieren
+        }
+
+        private void ExitButton_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void MainForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            SaveQuickLaunchItems();
+        }
+    }
+
+    [Serializable]
+    public class QuickLaunchItem
+    {
+        public string FilePath { get; set; }
+        public string FileName { get; set; }
+    }
+}
